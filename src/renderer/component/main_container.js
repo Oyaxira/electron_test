cfd96ef06af6a6148a5ee15266fc58ad256c0d21
oyaxira <oyaxira@vip.qq.com>
2019-07-15 16:03:04 +0800

    修正编译乱码

--- src/renderer/component/main_container.js
+++ src/renderer/component/main_container.js
@@ -5,17 +5,23 @@ import http from '../until/http'
 export default class MainContainer extends Component {
   constructor(props){
     super(props)
+    this.state = {}
   }
 
   componentDidMount() {
     http.get("https://youzhao.io").then(resp => {
-      console.log(resp.data)
+      this.setState({
+        data: resp.data
+      })
     })
   }
 
   render() {
     return <div className="main-container">
       <h1>露露露</h1>
+      {
+        this.state.data && <div>{this.state.data}</div>
+      }
     </div>
   }
 }
